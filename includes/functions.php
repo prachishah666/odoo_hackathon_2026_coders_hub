e8c120159bf784ea44a70cf89d3dcebbdc2119f9
prachishah666 <prachishah.24.ce@iite.indusuni.ac.in>
2026-07-12 11:23:45 +0530

    footer functions and header.php added

--- includes/functions.php
+++ includes/functions.php
@@ -5,4 +5,20 @@ function clean($data)
     return htmlspecialchars(trim($data));
 }
 
+function redirect($page)
+{
+    header("Location: $page");
+    exit();
+}
+
+function isLoggedIn()
+{
+    return isset($_SESSION['user_id']);
+}
+
+function showAlert($message,$type="danger")
+{
+    return "<div class='alert alert-$type'>$message</div>";
+}
+
 ?>
